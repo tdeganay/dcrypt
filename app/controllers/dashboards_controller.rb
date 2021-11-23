83d04752c5e5b5da4abec66c6b57c3a0f5c3ee23
charles brotons <chbrotons@hotmail.com>
2021-11-23 11:39:01 +0100

    added pundit partially

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,17 @@
+class DashboardsController < ApplicationController
+  def show
+    authorize @dashboard
+  end
+
+  def update?
+    record.user == user
+  end
+
+  # def index
+  #   @dashboards = policy_scope(dashboard).order(created_at: :desc)
+  # end
+
+  # def destroy?
+  #   record.user == user
+  # end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_154217) do
+ActiveRecord::Schema.define(version: 2021_11_23_103132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dashboards", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_dashboards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +30,10 @@ ActiveRecord::Schema.define(version: 2021_11_22_154217) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dashboards", "users"
 end

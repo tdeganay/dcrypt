841ba8d6f571f20429296e4f2073017dde0aad09
antoine pecheux <ant.pecheux@gmail.com>
2021-11-23 13:08:05 +0100

    routes draft done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'twitter_block/show'
-  get 'twitter_block/new'
-  get 'twitter_block/create'
-  get 'twitter_block/edit'
-  get 'twitter_block/update'
-  get 'twitter_block/destroy'
-  resources :dashboards, only: [:show, :new, :create]
+  resources :dashboards, only: [:show, :new, :create] do
+    resources :twitter_blocks, only: [:show, :new, :create, :update, :destroy]
+    resources :news_blocks, only: [:show, :new, :create, :update, :destroy]
+    resources :key_figures_blocks, only: [:show, :new, :create, :update, :destroy]
+  end
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

2c066c8d8bd9085806755b654bc79b880afcf37f
Mathieu Courtois <kourtois@protonmail.com>
2021-11-26 15:09:39 +0100

    navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,13 @@ class User < ApplicationRecord
 
   regex_email = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i
   validates :email, uniqueness: { case_sensitive: false }, format: { with: regex_email }, presence: true
-  #validates :username, presence: true
+  # validates :username, presence: true
   has_many :dashboards
   has_many :twitter_blocks, through: :dashboards
   has_many :news_blocks, through: :dashboards
   has_many :key_figures_blocks, through: :dashboards
+
+  def dashboard
+    dashboards.first
+  end
 end
